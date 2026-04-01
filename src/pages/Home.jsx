d2f08cc41bddfaef7d45c3d4f633568e5e4db81b
yegost <caza.alexandru@yahoo.com>
2026-04-01 23:59:34 +0100

    usenav change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import NavBar from "../components/NavBar";
+import SearchBar from "../components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const [query, setQuery] = useState('')
+
     return(
-        <NavBar />
+        <>
+            <NavBar />
+            <section>
+                <p>discover cinema</p>
+                <h1>What's on your mind?</h1>
+                <SearchBar 
+                    onSearch={setQuery}
+                 />
+            </section>
+        </>
     )
 }
 
