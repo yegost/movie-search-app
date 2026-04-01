7a5f00847118e6ec9fe27b5f14ca13f09266daca
yegost <caza.alexandru@yahoo.com>
2026-04-01 22:57:53 +0100

    add placeholder for posters

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,10 @@ function MovieCard({ movie }) {
         <>
             <div className="movie-card">
                 <h3>{movie.title}</h3>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <img src={movie.poster_path 
+                    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+                    : ".//posterPlaceholder.png"} alt={movie.title} 
+                />
                 <p>{movie.vote_average !== 0 ? "Rating: " + movie.vote_average.toFixed(1) : ''}</p>
                 <p>{movie.release_date.slice(0,4)}</p>
             </div>
