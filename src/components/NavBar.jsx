4d4cae35ad0475d9250694a8d0a5bbefd870e38c
yegost <caza.alexandru@yahoo.com>
2026-04-02 08:51:37 +0100

    add header to favorites

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,20 +17,20 @@ function NavBar() {
             : "text-zinc-400 hover:text-red-800"
 
     return(
-        <header className="bg-zinc-950 border-b border-zinc-800 flex items-center px-8 py-6">
-            <div className="mr-10">
+        <header className="bg-zinc-950 border-b border-zinc-800 flex items-center justify-between px-8 py-6">
+            <div className="mr-10 hidden sm:block">
                 <Link to="/"><h2 className="italic text-red-500 tracking-tight">MOVIE SEARCH</h2></Link>
             </div>
-            <div className="space-x-6 text-xs tracking-widest font-semibold">
+            <div className="hidden md:flex m-5 space-x-6 text-xs tracking-widest font-semibold">
                 <Link to="/" className={linkClass("/")}>HOME</Link>
                 <Link to="/favorites" className={linkClass("/favorites")}>FAVORITES</Link>
             </div>
             {location.pathname !== "/" && (
-                <div className="ml-auto">
+                <div className="sm:ml-auto mx-auto sm:mx-0">
                     <SearchBar 
                         onSearch={setQuery} 
                         onKeyDown={handleSearch} 
-                        className="text-sm px-4 py-2 bg-zinc-800 text-white rounded-lg w-72 border border-zinc-600 focus:outline-none focus:border-red-500 focus:shadow-[0_0_10px_rgba(239,68,68,0.4)] transition-al duration-200"
+                        className="text-sm px-4 py-2 bg-zinc-800 text-white rounded-lg w-72 border border-zinc-600 focus:outline-none focus:border-red-500 focus:shadow-[0_0_10px_rgba(239,68,68,0.4)] transition-all duration-200"
                         placeholder="Search movies..."
                     />
                 </div>
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,7 +2,28 @@ import NavBar from "../components/NavBar";
 
 function Favorites() {
     return(
-        <NavBar />
+        <>
+            <div className="min-h-screen bg-zinc-950 flex flex-col">
+                <NavBar />
+                <section className="relative bg-zinc-950 px-6 md:px-16 py-12 pb-0 overflow-hidden">
+                    <div className="absolute top-0 left-0 right-0 h-32 bg-gradient-to-b from-zinc-800/40 to-transparent" />
+                    <div className="relative z-10 max-w-5xl mx-auto flex border-b border-zinc-800 items-center">
+                        <div className="text-white flex-1">
+                            <p className="text-yellow-400 text-xs tracking-widest mb-3">CURATED</p>
+                            <h1 className="tracking-wide font-black mt-4 mb-3">Your Favorites</h1>
+                            <p className="text-zinc-500 text-xs tracking-widest pb-12 max-w-md">A personal gallery of cinematic masterpieces and stories that moved you. High-definition visual storytelling at your fingertips.</p>
+                        </div>
+                        <div className="hidden sm:block ml-auto pb-10">
+                            <img 
+                                src={`/hero.jpg`} 
+                                alt="Absolute Cinema" 
+                                className="w-50 h-40 object-cover rounded-lg shadow-2xl border-2 border-zinc-700 opacity-90"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
