3cd04d57d5a905397e61afd2d0831f8ac8d31e0e
yegost <caza.alexandru@yahoo.com>
2026-04-07 22:53:34 +0100

    add person details hook

--- src/hooks/usePersonDetail.js
+++ src/hooks/usePersonDetail.js
@@ -1,1 +1,35 @@
 import { useState, useEffect } from "react";
+
+function usePersonDetail(id) {
+    const [person, setPerson] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchPerson = async () => {
+            setLoading(true)
+
+            try {
+                const response = await fetch(
+                    `https://api.themoviedb.org/3/person/${id}?append_to_response=movie_credits`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
+                        }
+                    }
+                )
+                const data = await response.json()
+                setPerson(data)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchPerson()
+    }, [id])
+    
+    return { person, loading, error }
+}
+
+export default usePersonDetail
